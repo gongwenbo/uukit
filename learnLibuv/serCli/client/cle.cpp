2bab88a1e32c7a82a4db30e2cfc0a7d24724c383
gongwenbo <gongwenbotarsbot@gmail.com>
2017-11-18 04:07:41 -0800

    modify libuv tcp sever and client

--- learnLibuv/serCli/client/cle.cpp
+++ learnLibuv/serCli/client/cle.cpp
@@ -3,14 +3,36 @@
 #include <string.h>
 #include <uv.h>
 #include <iostream>
-using namespace std;
+#include <stdarg.h>
+#include <unistd.h>
 
-void debug(char* msg) {
-	printf("%s::%d:%s\n", __func__,__LINE__,msg);
+using namespace std;  
+
+uv_loop_t *loop;
+uv_timer_t tim_loop;
+uv_tcp_t client;
+struct sockaddr_in req_addr;
+uv_connect_t connect_req;
+uv_stream_t* tcp;
+uv_work_t req_work;
+
+
+
+int debug(const char* f,const int l,const char *format,...){
+	va_list arg;
+	va_start(arg,format);
+	fprintf(stderr,"debug:func:%s line:%d:",f,l);
+	vfprintf(stderr,format,arg);  /* 注意这里用的是vfprintf ,而不是printf */
+	fprintf(stderr,"\n");
+
+	va_end(arg);
+	return 0;
 }
+
 
 void on_connect(uv_connect_t *req, int status);
 void on_write_end(uv_write_t *req, int status);
+void timer_cb(uv_timer_t* handle);
 
 void echo_read(uv_stream_t *server, ssize_t nread, const uv_buf_t* buf) {
   if (nread == -1) {
@@ -34,9 +56,36 @@ void on_write_end(uv_write_t *req, int status) {
     fprintf(stderr, "error on_write_end");
     return;
   }
-
+  
   uv_read_start(req->handle, alloc_buffer, echo_read);
+  cout<<"on_write_end"<<endl;
+  
 }
+
+
+void AfterWork(uv_work_t *req,int status){
+
+	fprintf(stderr,"complite AfterWork\n");
+
+
+}
+void DataRS(uv_work_t *req){
+
+	uv_stream_t* tcp_back=(uv_stream_t*)req->data;
+	uv_write_t *write_req=(uv_write_t*)malloc(sizeof(uv_write_t));	
+	char message[20] = "hello.txt";
+	int len = strlen(message);
+	char buffer[100];
+	int buf_count = 1;	
+	uv_buf_t buf = uv_buf_init(buffer, sizeof(buffer));
+	
+	buf.len = len;
+	buf.base = message;
+	
+	uv_write(write_req, tcp_back, &buf, buf_count, on_write_end);
+	free(write_req);
+}
+
 
 void on_connect(uv_connect_t *req, int status) {
 	if (status == -1) {
@@ -44,42 +93,47 @@ void on_connect(uv_connect_t *req, int status) {
 		return;
 	}	
 
+	tcp= req->handle;
+	#if 0
+	uv_write_t write_req;
 	char message[20] = "hello.txt";
 	int len = strlen(message);
-
-	/*
-   * uv_buf_t buf[1];
-   * buf[0].len = len;
-   * buf[0].base = message;
-   */
-
 	char buffer[100];
+	int buf_count = 1;
+	
 	uv_buf_t buf = uv_buf_init(buffer, sizeof(buffer));
 	buf.len = len;
 	buf.base = message;
-
-	uv_stream_t* tcp = req->handle;
-
-	uv_write_t write_req;
-
-	int buf_count = 1;
-  
+	
 	uv_write(&write_req, tcp, &buf, buf_count, on_write_end);
+	#endif
+	
+	req_work.data=(void*)(req->handle);
+	
+	
 }
+
+void timer_cb(uv_timer_t* handle){
+
+	debug(__func__,__LINE__,"timer_cb test");
+	uv_queue_work(loop,&req_work,DataRS,AfterWork);
+
+}
+
 
 int main(void) {
-
   
-	uv_tcp_t client;
-	uv_loop_t *loop;
 	loop = uv_default_loop();
 	uv_tcp_init(loop, &client);
-
-	struct sockaddr_in req_addr;
+	uv_timer_init(loop,&tim_loop);
+	
 	uv_ip4_addr("127.0.0.1",8888,&req_addr);
-
-	uv_connect_t connect_req;
 	uv_tcp_connect(&connect_req, &client, (const struct sockaddr*)&req_addr, on_connect);
-
+	
+	uv_timer_start(&tim_loop, timer_cb, 0, 1000);
+		
 	return uv_run(loop,UV_RUN_DEFAULT);
 }
+
+
+
--- learnLibuv/serCli/server/ser.cpp
+++ learnLibuv/serCli/server/ser.cpp
@@ -1,18 +1,22 @@
 //date 11.17 by gong
 
-#include <stdio.h>                                                                                
-#include <stdbool.h>
+#include <stdio.h>  
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include <time.h>
 #include <sys/socket.h>
 #include <uv.h>
 #include <stdarg.h>
+#include <iostream>
+using namespace std;
 
 #define DEFAULT_PORT    8888
 #define DEFAULT_BACKLOG 1024
 
 uv_loop_t *loop = NULL;
 
+void on_write_end(uv_write_t *req, int status);
 int debug(const char* f,const int l,const char *format,...){
 	va_list arg;
 	va_start(arg,format);
@@ -30,13 +34,37 @@ void alloc_buffer(uv_handle_t *h, size_t size, uv_buf_t *buf) {
     buf->len = size;
 }
 
-void echo_read(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
+void echo_read(uv_stream_t *client_stream, ssize_t nread, const uv_buf_t *buf) {
     
     if (nread == UV_EOF) {
         printf("<EOF>\n");
     }
 	debug(__func__,__LINE__,"echo_read:%s\n",buf->base);
-    //printf("return data:%s\n", buf->base);
+
+	uv_write_t write_req;
+	char message[20] = "back hello.txt";
+	int len = strlen(message);
+	char buffer[100];
+	int buf_count = 1;
+	
+	uv_buf_t buf_back = uv_buf_init(buffer, sizeof(buffer));
+	buf_back.len = len;
+	buf_back.base = message;
+		
+	uv_write(&write_req, client_stream, &buf_back, buf_count, on_write_end);
+	cout<<"server uv_write "<<endl;
+
+
+	
+}
+
+
+void on_write_end(uv_write_t *req, int status) {
+  if (status == -1) {
+    fprintf(stderr, "error on_write_end");
+    return;
+  }
+
 }
 
 void on_new_connection(uv_stream_t *server, int status) {
@@ -51,18 +79,19 @@ void on_new_connection(uv_stream_t *server, int status) {
         uv_read_start((uv_stream_t*)client, alloc_buffer, echo_read);
     } else {
         uv_close((uv_handle_t*)client, NULL);
+		return ;
     }
+
+
 }
 
 int main() {
-    
-	
+    	
     loop = uv_default_loop();
-
     uv_tcp_t server;
-    uv_tcp_init(loop, &server);
-	
 	struct sockaddr_in addr;
+	
+    uv_tcp_init(loop, &server);
 	
     printf("Running on port %d\n", DEFAULT_PORT);
     uv_ip4_addr("0.0.0.0", DEFAULT_PORT, &addr);
