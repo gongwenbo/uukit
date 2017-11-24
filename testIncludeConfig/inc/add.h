80b85627cacc62b2b3d7975a41e1b57d6be18522
gongwenbo <gongwenbotarsbot@gmail.com>
2017-11-24 18:11:52 +0800

    add cUrl and cond test

---
+++ testIncludeConfig/inc/add.h
@@ -0,0 +1,9 @@
+#ifndef ADD_H
+#define ADD_H
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int add (int a,int b);
+
+#endif 
---
+++ testIncludeConfig/main.cpp
@@ -0,0 +1,9 @@
+#include "add.h"
+
+int main(int argc,char* argv[]){
+
+	printf("return:%d",add(2,9));
+	return 0;
+
+
+}
