4e3f7e73c580834ab7c625f1d87f91ec54b46d95
gongwenbo <gongwenbotarsbot@gmail.com>
2017-11-24 14:16:41 +0800

    add condTest

---
+++ condTest/example/cond2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>           // std::cout
+#include <thread>             // std::thread
+#include <chrono>             // std::chrono::seconds
+#include <mutex>              // std::mutex, std::unique_lock
+#include <condition_variable> // std::condition_variable, std::cv_status
+
+std::condition_variable cv;
+
+int value;
+
+void do_read_value()
+{
+    std::cout<<"enter your data"<<std::endl;
+	std::cin >> value;   //进程阻塞，除非输入数据
+    cv.notify_one();
+}
+
+int main ()
+{
+    std::cout << "Please, enter an integer (I'll be printing dots): \n";
+    std::thread th(do_read_value);
+
+    std::mutex mtx;
+    std::unique_lock<std::mutex> lck(mtx);
+    while (cv.wait_for(lck,std::chrono::seconds(1)) == std::cv_status::timeout) {
+        std::cout << '.';
+        std::cout.flush();
+    }
+
+    std::cout << "You entered: " << value << '\n';
+
+    th.join();
+    return 0;
+}
---
+++ condTest/readme.txt
@@ -0,0 +1,2 @@
+这是一个简单的条件变量使用，c++的库还是比较容易实现。
+参考：https://github.com/forhappy/Cplusplus-Concurrency-In-Practice/blob/master/zh/chapter5-Condition-Variable/5.2%20Condition-variable-tutorial.md
